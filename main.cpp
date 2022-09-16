88d50f3a85f30f9cb1e53af48137705755e7d833
Heloisa Santos <heloisacmk@hotmail.com>
2022-09-16 16:09:51 -0700

    Update main.cpp
    soh deixando bonitinho

--- main.cpp
+++ main.cpp
@@ -8,9 +8,13 @@ int main()
 {
     string board[8][8]; //it creates 
     int rowMain, colMain; //local variables
+
     cout << "Your current position on the board (input first row, then column): ";
     cin >> rowMain >> colMain;
+
     Build player; //Define an instance of the Build Class
+
     player.legalMoves(board, rowMain, colMain);
+
     return 0;
 }
