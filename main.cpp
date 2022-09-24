89da8fc9d3947a71a26fc3b44e42f9e00de78978
Heloisa Santos <heloisacmk@hotmail.com>
2022-09-24 09:13:26 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,15 +6,46 @@ using namespace std;
 
 int main()
 {
-    string board[8][8]; //it creates 
-    int rowMain, colMain; //local variables
-
-    cout << "Your current position on the board (input first row, then column): ";
-    cin >> rowMain >> colMain;
+    int userRow, userCol; //local variables
 
     Build player; //Define an instance of the Build Class
 
-    player.legalMoves(board, rowMain, colMain);
+    cout << "Your current position on the board (input first row, then column): ";
+
+    // 1. get input
+    cin >> userRow >> userCol;
+    
+
+    // 2. show position on board
+    player.showInitialPosition();
+
+    // 3. show legal moves
+    player.legalMoves(userRow, userCol);
+
+    // 4. ask for next position
+    int menuChoice;
+    do {
+        cout << "Enter 1 to keep playing this game" << endl;
+        cout << "Enter 2 to start new game" << endl;
+        cout << "Enter 3 to exit" << endl;
+        cin >> menuChoice;
+
+        switch (menuChoice)
+        {
+        case 1:
+            player.continueGame();
+            break;
+
+        case 2:
+            player.restart();
+            break;
+
+        case 3:
+            cout << "Thank you for playing!";
+            break;
+        }
+    }
+    while (menuChoice != 3)
 
     return 0;
 }
